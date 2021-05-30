f28f567eb3d9ba378c3745aab22b72b75d12b304
syoung125 <laura120555@gmail.com>
2021-05-30 15:19:44 +0900

    build: :wrench: add dependencies, moduleAliases

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
+import Reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${Reset}
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>KSCode</title>
+        <meta name="description" content="Ko Seoyoung Coding Blog" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </div>
+  );
 }
-export default MyApp
+export default MyApp;
