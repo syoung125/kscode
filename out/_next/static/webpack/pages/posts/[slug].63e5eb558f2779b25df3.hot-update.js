6fb5165739271dd72a31c86a0dcf4088e97bae8f
syoung125 <laura120555@gmail.com>
2021-06-02 00:48:56 +0900

    Deploy Next.js to gh-pages

---
+++ out/_next/static/webpack/pages/posts/[slug].63e5eb558f2779b25df3.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate_N_E("pages/posts/[slug]",{
+
+/***/ "./src/content/posts/welcome.md":
+/*!**************************************!*\
+  !*** ./src/content/posts/welcome.md ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (\"---\\ntitle: \\\"Welcome to Seoyoung's blog\\\"\\ndate: \\\"2021.05.31\\\"\\n---\\n\\n# title\\n\\n**Hello world**, this is my first Next.js blog post and it is written in Markdown.\\nI hope you like it!\\n\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vc3JjL2NvbnRlbnQvcG9zdHMvd2VsY29tZS5tZD8xZTlhIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQWUsOFBBQStMIiwiZmlsZSI6Ii4vc3JjL2NvbnRlbnQvcG9zdHMvd2VsY29tZS5tZC5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IFwiLS0tXFxudGl0bGU6IFxcXCJXZWxjb21lIHRvIFNlb3lvdW5nJ3MgYmxvZ1xcXCJcXG5kYXRlOiBcXFwiMjAyMS4wNS4zMVxcXCJcXG4tLS1cXG5cXG4jIHRpdGxlXFxuXFxuKipIZWxsbyB3b3JsZCoqLCB0aGlzIGlzIG15IGZpcnN0IE5leHQuanMgYmxvZyBwb3N0IGFuZCBpdCBpcyB3cml0dGVuIGluIE1hcmtkb3duLlxcbkkgaG9wZSB5b3UgbGlrZSBpdCFcXG5cIjsiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/content/posts/welcome.md\n");
+
+/***/ })
+
+})
