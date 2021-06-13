010b4ad2c42c3dafdd7f53a198eb14dcd3649381
syoung125 <laura120555@gmail.com>
2021-06-13 21:36:02 +0900

    feat: :lipstick: add List color

--- src/asset/colors/activity-bar.ts
+++
@@ -1,5 +0,0 @@
-export const ActivityBar = {
-  background: "#333333",
-  foreground: "#ffffff",
-  inactiveForeground: "rgba(255,255,255,0.4)",
-};
--- src/asset/colors/index.ts
+++ src/asset/colors/index.ts
@@ -1,3 +1,12 @@
 export * from "./palette";
 
-export * from "./activity-bar";
+export const ActivityBar = {
+  background: "#333333",
+  foreground: "#ffffff",
+  inactiveForeground: "rgba(255,255,255,0.4)",
+};
+
+export const List = {
+  focusBackground: "rgb(80,80,80)",
+  hoverBackground: "rgb(50,50,50)",
+};
