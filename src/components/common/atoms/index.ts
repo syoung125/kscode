1d93c1a2ad89aadd1d347200667e039db197ce4a
syoung125 <laura120555@gmail.com>
2021-08-14 15:36:46 +0900

    feat: :sparkles: add new atoms/p component

--- src/components/common/atoms/index.ts
+++ src/components/common/atoms/index.ts
@@ -1,3 +1,6 @@
 export { default as AccordionItem } from "./AccordionItem";
 export { default as ActionItem } from "./ActionItem";
+
 export * from "./list-item";
+
+export { default as P } from "./P";
