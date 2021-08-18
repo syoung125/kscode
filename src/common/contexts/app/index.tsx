801c280c5d3ec1c4e84acbb54ef2f14e4334ee75
syoung125 <laura120555@gmail.com>
2021-08-18 10:32:22 +0900

    fix: :bug: select last post when close current post
    go to '/' when openPostPath is empty

--- src/common/contexts/app/index.tsx
+++ src/common/contexts/app/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/dist/client/router";
-import { useState, createContext, useContext } from "react";
+import { useState, createContext, useContext, useEffect } from "react";
 
 import { IAppContext } from "./IAppContext";
 
@@ -21,9 +21,26 @@ const AppContextProvider = ({
   const [openPostPaths, setOpenPostPaths] = useState<string[]>([]);
   const [currentPostPath, setCurrentPostPath] = useState<string | null>(null);
 
+  useEffect(() => {
+    if (!currentPostPath) {
+      return;
+    }
+
+    router.push(`/posts/${currentPostPath}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPostPath]);
+
+  useEffect(() => {
+    if (openPostPaths.length === 0) {
+      setCurrentPostPath(null);
+      router.push(`/`);
+      return;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [openPostPaths.length]);
+
   const selectPost = (path: string) => {
     setCurrentPostPath(path);
-    router.push(`/posts/${path}`);
 
     if (openPostPaths.find((_path) => _path === path) !== undefined) {
       return;
@@ -34,6 +51,11 @@ const AppContextProvider = ({
   const closePost = (path: string) => {
     const newOpenPostPaths = openPostPaths.filter((_path) => _path !== path);
     setOpenPostPaths(newOpenPostPaths);
+
+    if (path === currentPostPath && newOpenPostPaths.length !== 0) {
+      const nextCurrPostPath = newOpenPostPaths[newOpenPostPaths.length - 1];
+      selectPost(nextCurrPostPath);
+    }
   };
 
   const appStore: IAppContext = {
