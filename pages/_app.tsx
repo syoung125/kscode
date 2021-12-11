f5fe45c21f2d24fea1375364885078b00f0293d7
syoung125 <laura120555@gmail.com>
2021-12-11 12:10:35 +0900

    refactor: :recycle: divdie GlobalStyle to styles/global.style.ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,11 @@
 import Head from "next/head";
 import type { AppProps } from "next/app";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import reset from "styled-reset";
+import { ThemeProvider } from "styled-components";
 
+import GlobalStyle from "@src/common/styles/global.style";
 import { dark } from "@src/common/themes";
 import { AppContextProvider } from "@src/common/contexts/app";
 import PostService from "@src/common/services/post.service";
-
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-  html, body {
-    margin: 0;
-    padding: 0;
-    height: 100%;
-    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
-  }
-  body {
-    font-size: 1rem;
-    width: 100%;
-  }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-  input {
-    -webkit-appearance: none;
-    -webkit-border-radius: 0;
-  }
-  ul, li {
-    margin: 0;
-    padding: 0;
-    list-style-type: none; 
-  }
-  *,
-  ::after,
-  ::before {
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
-  }
-`;
 
 export type MyAppProps = AppProps & {
   postPaths: string[];
