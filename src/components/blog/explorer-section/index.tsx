36e8e550615acebb75def9bd2d27224f82a5402c
syoung125 <laura120555@gmail.com>
2021-12-15 03:28:19 +0900

    refactor: :truck: move file-tree to blog/explorer-section

--- src/components/blog/explorer-section/index.tsx
+++ src/components/blog/explorer-section/index.tsx
@@ -1,4 +1,3 @@
-import { FileTree } from "@src/components/common/organisms";
 import { AccordionSection } from "@src/components/common/molecules";
 
 import { useAppContext } from "@src/common/contexts/app";
@@ -6,6 +5,7 @@ import { useAppContext } from "@src/common/contexts/app";
 import { OPEN_POST_LIST_ITEM_HEIGHT } from "./open-post-list/open-post-list-item";
 
 import OpenPostList from "./open-post-list";
+import FileTree from "./file-tree";
 
 import Style from "./index.style";
 
