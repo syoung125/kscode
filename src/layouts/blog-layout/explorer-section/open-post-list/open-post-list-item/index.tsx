2cca402606ed8d6076a82d08cc0585efa4c21a2c
likelionSungGuk <sung9gi@gmail.com>
2022-10-26 22:51:11 +0900

    add: open posts, side-bar title pointer:cursor

--- src/layouts/blog-layout/explorer-section/open-post-list/open-post-list-item/index.tsx
+++ src/layouts/blog-layout/explorer-section/open-post-list/open-post-list-item/index.tsx
@@ -57,26 +57,28 @@ export default function OpenPostListItem({
 
 const Wrapper = styled.li<{ isSelected: boolean; height: string }>`
   ${({ height, isSelected, theme }) => `
-  display: flex;
-  flex-direction: Row;
-  align-items: center;
+    display: flex;
+    flex-direction: Row;
+    align-items: center;
 
-  height: ${height};
-  padding: 0 1.2rem;
+    height: ${height};
+    padding: 0 1.2rem;
 
-  ${
-    isSelected
-      ? `background-color: ${theme.colors.scheme.$gray200};`
-      : `&:hover { 
-            background-color: ${theme.colors.scheme.$gray300};
-         }`
+    ${
+      isSelected
+        ? `background-color: ${theme.colors.scheme.$gray200};`
+        : `&:hover { 
+              background-color: ${theme.colors.scheme.$gray300};
+          }`
+    }
+  `}
+  &:hover {
+    cursor: pointer;
   }
-`}
 `;
 
 const Title = styled.p`
   padding-left: 0.4rem;
-
   font-size: 0.8rem;
   text-overflow: ellipsis;
   white-space: nowrap;
