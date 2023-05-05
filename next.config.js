035ccc59ed8fc1e03e14f5e9c89350e8661e9990
syoung125 <laura120555@gmail.com>
2023-05-05 22:54:30 +0900

    feat: :arrow_up: react, next 외 다수 라이브러리 버전 업그레이드, yarn berry 적용 제거

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  target: "serverless",
   webpack: function (config) {
     config.module.rules.push({ test: /\.md$/, use: "raw-loader" });
     config.module.rules.push({ test: /\.yml$/, use: "raw-loader" });
