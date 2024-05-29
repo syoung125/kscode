f29e0bf05a48755f587eb4ba75c85492018c0577
syoung125 <laura120555@gmail.com>
2024-05-29 22:00:14 +0900

    refactor: render PostListPage to ssr

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,7 +1,6 @@
-import { useState, useEffect } from "react";
-
 import { useRouter } from "next/router";
 import styled from "styled-components";
+import { GetServerSidePropsContext } from "next";
 
 import ScrollPagenation from "@src/components/common/scroll-pagenation";
 import PostListItem from "@src/components/post-list/post-list-item";
@@ -10,18 +9,13 @@ import { breakpoints } from "@src/styles/theme";
 import PostService from "@src/services/post.service";
 import { Post } from "@src/types/post.type";
 
-export default function PostListPage() {
+type PostListPageProps = {
+  posts: Post[];
+};
+
+export default function PostListPage({ posts }: PostListPageProps) {
   const router = useRouter();
   const tag = String(router.query.tag);
-
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    (async () => {
-      const posts: Post[] = await PostService.getPosts({ tag });
-      setPosts(posts);
-    })();
-  }, [tag]);
 
   return (
     <Container>
@@ -33,6 +27,13 @@ export default function PostListPage() {
       </Wrapper>
     </Container>
   );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const tag = String(context.query.tag);
+  const posts: Post[] = await PostService.getPosts({ tag });
+
+  return { props: { posts } };
 }
 
 const Container = styled.div`
